a9c2831f44d77f71bbf4d6c1228e87ec432fe9a9
fatih <>
2017-12-16 16:53:03 +0300

    first commit

---
+++ Landmark Book/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Landmark Book
+//
+//  Created by fatih on 16.12.2017.
+//  Copyright © 2017 fatih. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
